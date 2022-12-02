aea84e761192a8882769b228c5942f46534e3de7
juraganelite <101795987+juraganelite@users.noreply.github.com>
2022-12-02 08:35:07 +0700

    Create kumprung.js

---
+++ kumprung.js
@@ -0,0 +1,12 @@
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    //ubah google dengan ads direct link
+	    window.open('https://bringsconserve.com/imc6e3ki0?key=05399f3daa3b926aa8429bf2266fc1e3', '_blank');
+	});
